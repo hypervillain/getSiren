58c1c222c3d175db305593c82b39a481d8f6532d
lodi-g <gregoire.lodi@gmail.com>
2017-08-25 23:17:16 +0200

    Forgot to run eslint on the test file

--- test.js
+++ test.js
@@ -7,4 +7,4 @@ getSiren.requestDataFromSiren(process.argv[2] || '38347481400043')
   .catch((error) => {
     console.log(`status : ${error.code}`);
     console.log(`log : ${error.log}`);
-});
+  });
